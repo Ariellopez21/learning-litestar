0971c44f6b18fcb97bc224dae3f9a69ab79da73e
Ariellopez21 <arilopez@umag.cl>
2025-06-05 10:54:44 -0300

    ERROR DE CORS CON FRONT END

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from litestar import Litestar
 from litestar.openapi import OpenAPIConfig
 from litestar.openapi.plugins import SwaggerRenderPlugin, ScalarRenderPlugin
+from litestar.config.cors import CORSConfig
 
 from app.controllers import TagController, TodoController, UserController, AuthController
 from app.db import sqla_plugin
@@ -13,12 +14,15 @@ openapi_config = OpenAPIConfig(
     version="0.9.9",
     render_plugins=[SwaggerRenderPlugin(),ScalarRenderPlugin()])
 
+cors_config = CORSConfig(allow_origins=["*"])
+
 # Una lista de funciones para recibir información de la API
 app = Litestar(
     route_handlers=[TodoController, UserController, TagController, AuthController], 
     openapi_config=openapi_config,
     plugins=[sqla_plugin],
     debug=settings.debug,
+    cors_config=cors_config,
     #on_app_init=[oauth2_auth.on_app_init],
     #pdb_on_exception=True # Es para debuggear desde VSCode: Ejecutar>Iniciar depuración
     )
